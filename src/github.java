bd3e25c76b4ca2f182ae8b6b6bd6dd8685108151
Lionel Messi <dperram3009g.educaan.es>
2022-12-12 16:37:14 +0100

    Comienzo

--- src/github.java
+++ src/github.java
@@ -1,12 +1,15 @@
+import java.util.Scanner;
 
 public class github {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola 1º DAM B");
-		String nombre = "Dario Perez";
-		System.out.println(nombre); 
-
-	}
+		Scanner sc = new Scanner(System.in);
+		int numero1, numero2, resultado;
+		System.out.print("Introduce primer número: ");
+		numero1 = sc.nextInt();
+		System.out.print("Introduce segundo número: ");
+		numero2 = sc.nextInt();
+		}
 
 }
