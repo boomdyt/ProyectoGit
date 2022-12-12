15d1a2aa0bbb45fe4b27625e0a8885e880d659ff
Lionel Messi <dperram3009g.educaan.es>
2022-12-12 16:18:13 +0100

    Paco

--- src/github.java
+++ src/github.java
@@ -3,6 +3,9 @@ public class github {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hola 1º DAM B");
+		String nombre = "Dario Perez";
+		System.out.println(nombre);
 
 	}
 
